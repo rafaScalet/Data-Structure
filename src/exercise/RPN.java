75de131aa85a43f25e92cbcf8b4501470838a03e
rafaScalet <rafaelscalet2018@gmail.com>
2024-08-30 16:42:07 -0300

    Exercise conversion to infix into postfix

--- src/exercise/RPN.java
+++ src/exercise/RPN.java
@@ -33,4 +33,53 @@ public class RPN {
 
 		return stack.pop();
 	}
+
+	public static String infixToPostfix (String infixExpression) {
+		Stack<Character> operatorStack = new Stack<>();
+		String postfixExpression = "";
+
+		for (int index = 0; index < infixExpression.length(); index++) {
+			char character = infixExpression.charAt(index);
+
+			if (Character.isAlphabetic(character)) {
+				postfixExpression += character;
+				continue;
+			}
+
+			if (character == '(') {
+				operatorStack.push(character);
+				continue;
+			}
+
+			if (character == ')') {
+				while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+					postfixExpression += operatorStack.pop();
+				}
+				operatorStack.pop();
+				continue;
+			}
+
+			while(!operatorStack.isEmpty() && getPrecedence(operatorStack.peek()) >= getPrecedence(character)) {
+				postfixExpression += operatorStack.pop();
+				continue;
+			}
+
+			operatorStack.push(character);
+		}
+
+		while (!operatorStack.isEmpty()) {
+			postfixExpression += operatorStack.pop();
+		}
+
+		return postfixExpression;
+	}
+
+	private static int getPrecedence (char operator) {
+		switch (operator) {
+			case '+': case '-': return 1;
+			case '*': case '/': return 2;
+			case '^': return 3;
+			default: return 0;
+		}
+	}
 }
