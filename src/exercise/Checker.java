7bd5995435e4b5e988289802cf3d9eba960c1fc9
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-03 09:30:51 -0300

    Exercise 'check if is palindrome'

--- src/exercise/Checker.java
+++ src/exercise/Checker.java
@@ -1,6 +1,7 @@
 package exercise;
 
 import data.Stack;
+import data.Queue;
 
 public class Checker {
 	public static boolean isBalanced (String expression) {
@@ -29,6 +30,26 @@ public class Checker {
 		}
 
 		return stack.isEmpty();
+	}
+
+	public static boolean isPalindrome (String expression) {
+		Stack<Character> stack = new Stack<>();
+		Queue<Character> queue = new Queue<>();
+
+		for (int index = 0; index < expression.length(); index++) {
+			char character = expression.charAt(index);
+
+			stack.push(character);
+			queue.add(character);
+		}
+
+		while (!queue.isEmpty()) {
+			if (queue.remove() != stack.pop()) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	private static boolean isMatching (char opened, char closed) {
